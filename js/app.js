b3b4104c1c0b601109f9349d432cf89827387ff5
Tolga Caglar <tolcaglar@gmail.com>
2021-04-07 21:29:23 -0700

    more website

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+function focusPothole(el) {
+    // Reset the pothol 
+    id = el.id;
+    // Focus on the chosen 
+}
 
+function resetPothole(el) {
+    // Resets the pothole layer to unfocus from all the visible potholes
+}
