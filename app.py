45f7f0e2e22f53b10e27085b31fc22a56c3e292e
Jeriel <jeriel.minguillan@gmail.com>
2021-04-09 01:23:55 -0700

    first db query

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgres://gjijwlitunzqrh:a3722514ebcbdb13b7548855c5794e8205c88e8c18e9fef14b989481433b517b@ec2-54-211-176-156.compute-1.amazonaws.com:5432/da9sl2d3dh1uah"
-#os.environ.get('DATABASE_URL', '') or "postgres://gjijwlitunzqrh:a3722514ebcbdb13b7548855c5794e8205c88e8c18e9fef14b989481433b517b@ec2-54-211-176-156.compute-1.amazonaws.com:5432/da9sl2d3dh1uah"
+
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+database = "da9sl2d3dh1uah"
 
 Pot = create_classes(db)
 
@@ -36,28 +37,37 @@ def home():
     return render_template("index.html")
 
 
-# Query the database and send the jsonified results
-#@app.route("/send", methods=["GET", "POST"])
-#def send():
-   # if request.method == "POST":
-      #  name = request.form["year_actual"]
-        # qn= request.form["quarter_name"]
-        # lon = request.form["month_actual"]
-
-       # pot = Pot(name=name)
-       # db.session.add(pot)
-      #  db.session.commit()
-      #  return redirect("/", code=302)
-
-   # return render_template("form.html")
-
 
 @app.route("/api/pothole_cy")
 def ping():
-    results = db.session.query(Pot.pothole_cy).all()
+    results = db.session.query(database.pothole_cy).all()
     print(results)
     
-    return jsonify(results)
+    return redirect(results)
+
+
+
+
+
+
+# @app.route("/api/pals")
+# def pals():
+#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+
+#     hover_text = [result[0] for result in results]
+#     lat = [result[1] for result in results]
+#     lon = [result[2] for result in results]
+
+#     pet_data = [{
+        
+#         "lat": lat,
+#         "lon": lon,
+#         }
+#     ]
+
+#     return jsonify(pet_data)
+
+
 
 
 if __name__ == "__main__":
