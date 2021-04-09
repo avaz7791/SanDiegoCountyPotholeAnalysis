f01389ec7b7dee887658142a50581dd6166f831e
Tolga Caglar <tolcaglar@gmail.com>
2021-04-08 19:52:11 -0700

    adding control.js for filtering and etc.

---
+++ js/controls.js
@@ -0,0 +1,20 @@
+
+var filterForm = d3.select("#filter-form")
+
+filterButton.on("submit", function() {
+    console.log("Filter form is called!")
+    
+})
+
+function focusPothole(el) {
+    // Reset the pothol 
+    id = el.id;
+    console.log(id)
+    // Focus on the chosen pothole in the map
+    
+}
+
+
+function resetPothole(el) {
+    // Resets the pothole layer to unfocus from all the visible potholes
+}
