ad439e58c5e19b0fdde239e5142cfd108d6f3370
Tolga Caglar <tolcaglar@gmail.com>
2021-04-10 02:22:10 -0700

    rending html files

---
+++ app.py
@@ -0,0 +1,93 @@
+#import necessary libraries
+# from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect,
+    url_for)
+
+
+# def create_classes(db):
+#     class Pet(db.Model):
+#         __tablename__ = 'pets'
+
+#         id = db.Column(db.Integer, primary_key=True)
+#         name = db.Column(db.String(64))
+#         lat = db.Column(db.Float)
+#         lon = db.Column(db.Float)
+
+#         def __repr__(self):
+#             return '<Pet %r>' % (self.name)
+#     return Pet
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgres://gjijwlitunzqrh:a3722514ebcbdb13b7548855c5794e8205c88e8c18e9fef14b989481433b517b@ec2-54-211-176-156.compute-1.amazonaws.com:5432/da9sl2d3dh1uah"
+#os.environ.get('DATABASE_URL', '') or "postgres://gjijwlitunzqrh:a3722514ebcbdb13b7548855c5794e8205c88e8c18e9fef14b989481433b517b@ec2-54-211-176-156.compute-1.amazonaws.com:5432/da9sl2d3dh1uah"
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Pot = create_classes(db)
+
+# create route that renders index.html template
+@app.route("/")
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+# Query the database and send the jsonified results
+#@app.route("/send", methods=["GET", "POST"])
+#def send():
+   # if request.method == "POST":
+      #  name = request.form["year_actual"]
+        # qn= request.form["quarter_name"]
+        # lon = request.form["month_actual"]
+
+       # pot = Pot(name=name)
+       # db.session.add(pot)
+      #  db.session.commit()
+      #  return redirect("/", code=302)
+
+   # return render_template("form.html")
+
+
+# @app.route("/api/pothole_cy")
+# def ping():
+#     results = db.session.query(Pot.pothole_cy).all()
+#     print(results)
+    
+#     return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/app.js
@@ -0,0 +1,105 @@
+// Amir
+
+function buildPlot() {
+
+  const url ='/api/pothole_cy';
+  d3.json(url).then(function(response){
+      console.log(response);
+
+      const data = response;
+
+      const layout = {
+        scope: "San Diego",
+        title: "Potholes",
+        showlegend: false,
+        height: 600,
+              // width: 980,
+        geo: {
+          scope: "usa",
+          projection: {
+            type: "San Diego"
+          },
+          showland: true,
+          landcolor: "rgb(217, 217, 217)",
+          subunitwidth: 1,
+          countrywidth: 1,
+          subunitcolor: "rgb(255,255,255)",
+          countrycolor: "rgb(255,255,255)"
+        }
+      };
+  
+      Plotly.newPlot("plot", data, layout);
+  });
+
+}
+
+
+//Amir
+
+
+
+
+
+
+function focusPothole(el) {
+    // Reset the pothol 
+    id = el.id;
+    // Focus on the chosen pothole in the map
+}
+
+
+function resetPothole(el) {
+    // Resets the pothole layer to unfocus from all the visible potholes
+}
+
+// Jeriel -------
+//border layer------------------------------
+var myMap = L.map("mapid", {
+  center: [32.7157, -117.1611],
+  zoom: 11
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Use this link to get the geojson data.
+var link = "{{ url_for('static', filename='council_districts_datasd.geojson') }}";
+console.log(link)
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(myMap);
+});
+//end border layer--------------------------------------------
+
+
+
+
+
+
+
+
+
+//var potholejson = "pothole_cy.json";
+
+// d3.json(potholejson, function(response) {
+
+//   console.log(response);
+
+//   for (var i = 0; i < response.length; i++) {
+//     var location = response[i].location;
+
+//     if (location) {
+//       L.marker([location.coordinates[0], location.coordinates[1]]).addTo(myMap);
+//     }
+//   }
+
+// });
+//Jeriel--------^
