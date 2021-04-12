143e6dc710c7305f8e5402f4cc233f0e146af6ba
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-04-11 21:50:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -109,8 +109,9 @@ def pothole_cy():
 
     return jsonify(pothole_data)
 
-# Weather CY
-
+@app.route("/council_districts_datasd")
+def council_districts_datasd():
+    return redirect(url_for("static", filename="geojson/council_districts_datasd.geojson"))
 
 
 if __name__ == "__main__":
