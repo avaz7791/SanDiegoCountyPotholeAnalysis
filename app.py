44332d714cf00476eb5a7ccd93d96818c323c85f
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-04-14 00:25:44 -0700

    test

--- app.py
+++ app.py
@@ -125,49 +125,6 @@ def sdcpa_data():
 
     return jsonify(data)
 
-@app.route("/api/sdcpa_summarydata")
-def sdcpa_summarydata():
-    sum_pothole_response = db.session.query(SumPotholeData.pksummaryid, SumPotholeData.year_actual, SumPotholeData.quarter_name, SumPotholeData.month_actual, \
-                                            SumPotholeData.month_name, SumPotholeData.status, SumPotholeData.cnt_new, SumPotholeData.cnt_inprogress, SumPotholeData.cnt_closed, SumPotholeData.cnt_referred, \
-                                            SumPotholeData.total_cnt,).all()
-    sum_weather_response = db.session.query(SumWeatherData.pksummaryid, SumWeatherData.year_actual, SumWeatherData.quarter_name, SumWeatherData.month_actual, \
-                                            SumWeatherData.month_name, SumWeatherData.dapr, SumWeatherData.mdpr, SumWeatherData.prcp, ).all()
-    #print(results) SumPotholeData, SumWeatherData
-
-    # All the data in a dictionary
-    data = {}
-
-    # Parse summary pothole data into a dictionary
-    SummaryPothole_data = []
-    for spd in sum_pothole_response:
-        SummaryPothole_data.append({"pksummaryid":spd[0],
-                            "year_actual": spd[1],
-                            "quarter_name": spd[2],
-                            "month_actual"    : spd[3],
-                            "month_name": spd[4],
-                            "status": spd[5],
-                            "cnt_new": spd[6],
-                            "cnt_inprogress" : spd[7],
-                            "cnt_closed": spd[8],
-                            "cnt_referred": spd[9],
-                            "total_cnt" : spd[10]})
-
-    # Parse summary weather data into a dictionary
-    SummaryWeather_data = []
-    for swd in sum_weather_response:
-        SummaryWeather_data.append({"pksummaryid":swd[0],
-                            "year_actual": swd[1],
-                            "quarter_name": swd[2],
-                            "month_actual"    : swd[3],
-                            "month_name": swd[4],
-                            "dapr": swd[5],
-                            "mdpr": swd[6],
-                            "prcp" : swd[7] })
-
-    data["summary_potholeAnalysis"] = SummaryPothole_data
-    data["summary_weatherAnalysis"] = SummaryWeather_data
-
-    return jsonify(data)
 
 
 if __name__ == "__main__":
