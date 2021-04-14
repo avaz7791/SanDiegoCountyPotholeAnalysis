2bd3c70791b57181064e80b3b10e27b72d62afb2
Jeriel <jeriel.minguillan@gmail.com>
2021-04-14 15:56:45 -0700

    popup testing

--- static/js/app.js
+++ static/js/app.js
@@ -95,7 +95,7 @@ d3.json("/api/sdcpa_data").then(function(response)
 
       // Add a new marker to the cluster group and bind a pop-up
       markers.addLayer(L.marker([phlocation[0], phlocation[1]])
-         .bindPopup("<h3>" + "TEST" + 24 + "</h3><h5>" + feature.properties.district + "</h5>"))
+         .bindPopup("<h3>" + "TEST" + 24 + "</h3><h5>" + response.potholes_cy[i].latitude + "</h5>"))
     }
     
   }
