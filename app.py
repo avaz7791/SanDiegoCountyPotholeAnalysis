5eac80fac807b01474ca5d7c4be7c2cbf17eaa0b
Tolga Caglar <tolcaglar@gmail.com>
2021-04-14 00:07:00 -0700

    app.py merge fix

--- app.py
+++ app.py
@@ -1,29 +1,4 @@
 #import necessary libraries
-<<<<<<< HEAD
-# from models import create_classes
-import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect,
-    url_for)
-
-
-# def create_classes(db):
-#     class Pet(db.Model):
-#         __tablename__ = 'pets'
-
-#         id = db.Column(db.Integer, primary_key=True)
-#         name = db.Column(db.String(64))
-#         lat = db.Column(db.Float)
-#         lon = db.Column(db.Float)
-
-#         def __repr__(self):
-#             return '<Pet %r>' % (self.name)
-#     return Pet
-=======
 try:
     from models import create_classes
 
@@ -44,7 +19,6 @@ try:
 
 except Exception as e:
         print(f'a module(s) have not been imported {e}')
->>>>>>> d27729d73794b28d6496d34eb6c0a257c10e7452
 
 #################################################
 # Flask Setup
@@ -55,25 +29,14 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-<<<<<<< HEAD
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgres://gjijwlitunzqrh:a3722514ebcbdb13b7548855c5794e8205c88e8c18e9fef14b989481433b517b@ec2-54-211-176-156.compute-1.amazonaws.com:5432/da9sl2d3dh1uah"
-#os.environ.get('DATABASE_URL', '') or "postgres://gjijwlitunzqrh:a3722514ebcbdb13b7548855c5794e8205c88e8c18e9fef14b989481433b517b@ec2-54-211-176-156.compute-1.amazonaws.com:5432/da9sl2d3dh1uah"
-=======
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace("://", "ql://", 1) or "postgresql://postgres:456789123@localhost:5432/sdc_pothole"
->>>>>>> d27729d73794b28d6496d34eb6c0a257c10e7452
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-<<<<<<< HEAD
-
-# Pot = create_classes(db)
-=======
 Pothole, Weather, SumPotholeData, SumWeatherData = create_classes(db)
->>>>>>> d27729d73794b28d6496d34eb6c0a257c10e7452
 
 # create route that renders index.html template
 @app.route("/")
@@ -97,31 +60,6 @@ def team():
 def project():
     return render_template("project.html")
 
-<<<<<<< HEAD
-# Query the database and send the jsonified results
-#@app.route("/send", methods=["GET", "POST"])
-#def send():
-   # if request.method == "POST":
-      #  name = request.form["year_actual"]
-        # qn= request.form["quarter_name"]
-        # lon = request.form["month_actual"]
-
-       # pot = Pot(name=name)
-       # db.session.add(pot)
-      #  db.session.commit()
-      #  return redirect("/", code=302)
-
-   # return render_template("form.html")
-
-
-# @app.route("/api/pothole_cy")
-# def ping():
-#     results = db.session.query(Pot.pothole_cy).all()
-#     print(results)
-    
-#     return jsonify(results)
-
-=======
 # @app.route("/council_districts_datasd")
 # def council_districts_datasd():
 #     return redirect()
@@ -186,7 +124,6 @@ def sdcpa_data():
     data["council_districts_datasd"] = districts_geojson_dict
 
     return jsonify(data)
->>>>>>> d27729d73794b28d6496d34eb6c0a257c10e7452
 
 if __name__ == "__main__":
     app.run()
