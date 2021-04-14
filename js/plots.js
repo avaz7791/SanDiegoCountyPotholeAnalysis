d3c084046e5067542a255eb72c85c511ee19eb1a
Kevin Mickey <kmickey@gmail.com>
2021-04-13 23:11:25 -0700

    Update plots.js
    Removed (?) from Y axis

--- js/plots.js
+++ js/plots.js
@@ -9,13 +9,13 @@ var summaryWeather = [];
  * Real data will be read from d3.json after jsonifying the data
  * to a route in app.py
  */
-// Unique years 
+// Unique years
 years = ["2016", "2017", "2018", "2019", "2020", "2021"];
 precipitationPerYear = [688.66, 1195.24, 938.52, 2130.25, 1311.2, 409.98];
 potholesPerYear = [4914,24100,11912,20748,14548,4023];
 
 // Unique months
-months = ["January", "February", "March", "April", "May", "June", 
+months = ["January", "February", "March", "April", "May", "June",
           "July", "August", "September", "October", "November", "December"];
 precipitationPerMonth = [1351.51,1410.98,974.88,538.95,289.89,27.87,
                         21.05,20.09,85.06,74.44,770.36,1108.77];
@@ -83,7 +83,7 @@ var dataPerMonth = [
       color: "rgb(255,0,0)"
      }
   ];
-  
+
   var layoutPerMonth = {
       title: "Potholes per month",
       yaxis: {
@@ -92,12 +92,12 @@ var dataPerMonth = [
           tickfont: {color: "rgb(0,0,255)"}
       },
       yaxis2: {
-          title: "Precipitation (?)",
+          title: "Precipitation",
           overlaying: "y",
           side: "right",
           titlefont: {color: "rgb(255,0,0)"},
           tickfont: {color: "rgb(255,0,0)"}
       }
   };
-  
+
   Plotly.newPlot("plotPerMonth", dataPerMonth, layoutPerMonth, config)
